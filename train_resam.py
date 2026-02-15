23acf193b2028dff83fb350b0fb42d29725e7cb3
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-02-15 17:10:33 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -26,7 +26,7 @@ from box import Box
 from datasets import call_load_dataset
 from utils.model import Model
 from utils.losses import DiceLoss, FocalLoss, Matching_Loss, cosine_similarity
-from utils.eval_utils import AverageMeter, validate, get_prompts, calc_iou
+from utils.eval_utils import AverageMeter, validate, get_prompts, calc_iou, validate_per_object_verbose
 from utils.tools import copy_model, create_csv, reduce_instances
 from utils.utils import *
 
