adbe99d354e3603dcf5afc869d9f31fb8a3c9bda
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-02-25 16:59:13 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -160,7 +160,7 @@ def train_resam(cfg: Box, fabric: L.Fabric, model: Model, optimizer: _FabricOpti
             for i,  (pred, ent) in enumerate( zip(pred_binary, entropy_maps)):
 
                 pred_w_overlap = ((pred[0]*invert_overlap_map[0]  ) )#    * ((1 - 0.1 * ent[0]))
-                ys, xs = torch.where(pred_w_overlap > 0.5)
+                ys, xs = torch.where(pred_w_overlap > 0.2)
                 if len(xs) > 0 and len(ys) > 0:
                     x_min, x_max = xs.min().item(), xs.max().item()
                     y_min, y_max = ys.min().item(), ys.max().item()
