f810dcf6d45e8e1214ef008221437751fa51fe75
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-02-26 19:24:58 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -158,7 +158,8 @@ def train_resam(cfg: Box, fabric: L.Fabric, model: Model, optimizer: _FabricOpti
 
                 
                 confidence_map = 1 - entropy_maps  # higher is more confident
-                threshold = confidence_map.mean() * 0.8  # scale by instance mean confidence
+                threshold = entropy_maps.mean() * 0.8  # scale by instance mean confidence
+                print(threshold)
                 pred_binary = (pred_stack * confidence_map > threshold).float()
 
                 # pred_binary = (((1 - entropy_maps) * (pred_stack)) > 0.5) .float()
