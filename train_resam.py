1d050408e810b244dc20c21716327c3e2eb64bad
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-02-26 19:56:28 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -160,7 +160,7 @@ def train_resam(cfg: Box, fabric: L.Fabric, model: Model, optimizer: _FabricOpti
                 confidence_map = 1 - entropy_maps  # higher is more confident
                 threshold = confidence_map.mean() * 0.8  # scale by instance mean confidence
  
-                pred_binary = (pred_stack * confidence_map > (1-threshold)).float()
+                pred_binary = (pred_stack * confidence_map > 0).float()
 
                 # pred_binary = (((1 - entropy_maps) * (pred_stack)) > 0.5) .float()
                 overlap_count = pred_binary.sum(dim=0)
