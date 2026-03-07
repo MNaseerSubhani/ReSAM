60c58861142bb8640381c0d225149e614308f9b6
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-03-07 16:15:12 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -262,8 +262,8 @@ def train_resam(cfg: Box, fabric: L.Fabric, model: Model, optimizer: _FabricOpti
                 beta = (4 / (1 + math.exp(-1.0 * (epoch - ((cfg.num_epochs + 1) / 2)))))
                 loss_total =  (20 * loss_focal +  loss_dice  + loss_iou)# + 0.1*loss_sim)   
 
-                if watcher.is_outlier(loss_total):
-                    continue
+                # if watcher.is_outlier(loss_total):
+                #     continue
                 fabric.backward(loss_total)
 
                 if analyze:
