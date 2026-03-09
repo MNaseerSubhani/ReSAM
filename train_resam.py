f7793c84ad6124e0991c53efd4057abe56cd9970
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-03-09 20:52:25 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -208,16 +208,16 @@ def train_resam(cfg: Box, fabric: L.Fabric, model: Model, optimizer: _FabricOpti
 
                 batch_feats = []  
 
-                for i, (pred_mask, soft_mask, iou_prediction, bbox) in enumerate(
-                        zip(pred_masks[0], soft_masks[0], iou_predictions[0], bboxes  )
+                for i, (pred_mask, soft_mask, iou_prediction) in enumerate(
+                        zip(pred_masks, soft_masks, iou_predictions  )
                     ):
                         soft_mask = (soft_mask > 0.).float()
                         # soft_mask = torch.sigmoid(soft_mask).detach()
                     
                         loss_focal += focal_loss(pred_mask, soft_mask, num_masks)  
                         loss_dice += dice_loss(pred_mask, soft_mask, num_masks)   
-                        batch_iou = calc_iou(pred_mask.unsqueeze(0), soft_mask.unsqueeze(0))
-                        loss_iou += F.mse_loss(iou_prediction.view(-1), batch_iou.view(-1), reduction='sum') / num_masks
+                        batch_iou = calc_iou(pred_mask, soft_mask)
+                        loss_iou += F.mse_loss(iou_prediction, batch_iou, reduction='sum') / num_masks
 
                 del  pred_masks, iou_predictions 
                 del pred_stack, overlap_map, invert_overlap_map
