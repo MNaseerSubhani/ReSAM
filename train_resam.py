0d0eb4b2d3af0635d66fe31220c91613cba5f7cd
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-03-09 12:47:44 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -40,25 +40,39 @@ import matplotlib. pyplot as plt
 
 
 
+# def process_forward(img_tensor, prompt, model):
+#     with torch.no_grad():
+#         _, masks_pred, _, _ = model(img_tensor, prompt)
+#     entropy_maps = []
+#     pred_ins = []
+#     eps=1e-8
+#     for i, mask_p in enumerate( masks_pred[0]):
+#         mask_p = torch.sigmoid(mask_p)
+#         p = mask_p.clamp(1e-6, 1 - 1e-6)
+#         if p.ndim == 2:
+#             p = p.unsqueeze(0)
+
+#         entropy = - (p * torch.log(p + eps) + (1 - p) * torch.log(1 - p + eps))
+#         max_ent = torch.log(torch.tensor(2.0, device=mask_p.device))
+#         entropy_norm = entropy / (max_ent + 1e-8)   # [0, 1]
+#         entropy_maps.append(entropy_norm)
+#         pred_ins.append(p)
+
+#     return entropy_maps, pred_ins
+
 def process_forward(img_tensor, prompt, model):
+
     with torch.no_grad():
         _, masks_pred, _, _ = model(img_tensor, prompt)
-    entropy_maps = []
-    pred_ins = []
-    eps=1e-8
-    for i, mask_p in enumerate( masks_pred[0]):
-        mask_p = torch.sigmoid(mask_p)
-        p = mask_p.clamp(1e-6, 1 - 1e-6)
-        if p.ndim == 2:
-            p = p.unsqueeze(0)
 
-        entropy = - (p * torch.log(p + eps) + (1 - p) * torch.log(1 - p + eps))
-        max_ent = torch.log(torch.tensor(2.0, device=mask_p.device))
-        entropy_norm = entropy / (max_ent + 1e-8)   # [0, 1]
-        entropy_maps.append(entropy_norm)
-        pred_ins.append(p)
+    masks = torch.sigmoid(masks_pred[0]).clamp(1e-6, 1 - 1e-6)
 
-    return entropy_maps, pred_ins
+    entropy = -(masks * torch.log(masks) +
+                (1 - masks) * torch.log(1 - masks))
+
+    entropy = entropy / math.log(2)   # normalize to [0,1]
+
+    return entropy, masks
         
 
 # persistent feature queue
