ed680985c2883d1a748a23a09967f23dbb093211
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-03-11 22:10:45 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -77,9 +77,11 @@ def process_forward(img_tensor, prompt, model):
     return entropy_maps, pred_ins
         
 
+
+len_q = 512
 # persistent feature queue
-feature_queue = deque(maxlen=32)  # keep up to 512 previous object embeddings
-feature_queue_hard = deque(maxlen=32)
+feature_queue = deque(maxlen=len_q)  # keep up to 512 previous object embeddings
+feature_queue_hard = deque(maxlen=len_q)
 
 
 
@@ -206,7 +208,7 @@ def train_resam(cfg: Box, fabric: L.Fabric, model: Model, optimizer: _FabricOpti
                 batch_feats_hard = [get_bbox_feature(hard_embeddings, bbox) for bbox in bboxes]
             
                 
-                if len(feature_queue) == 32:
+                if len(feature_queue) == len_q:
                     batch_feats = F.normalize(torch.stack(batch_feats, dim=0), dim=1)
                     batch_feats_hard = F.normalize(torch.stack(batch_feats_hard, dim=0), dim=1)
                     loss_sim = similarity_loss(feature_queue_hard,feature_queue)
@@ -261,7 +263,7 @@ def train_resam(cfg: Box, fabric: L.Fabric, model: Model, optimizer: _FabricOpti
                 loss_iou = loss_iou/num_masks
                 
                 beta = (4 / (1 + math.exp(-1.0 * (epoch - ((cfg.num_epochs + 1) / 2)))))
-                loss_total =  (20 * loss_focal +  loss_dice  + loss_iou +0.01*loss_sim)   
+                loss_total =  (20 * loss_focal +  loss_dice  + loss_iou +0.1*loss_sim)   
 
                 # if watcher.is_outlier(loss_total):
                 #     continue
