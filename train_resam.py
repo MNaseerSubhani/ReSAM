45da24755da6e642b772835bfa71c88afdee97a6
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-03-14 15:13:59 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -213,7 +213,8 @@ def train_resam(cfg: Box, fabric: L.Fabric, model: Model, optimizer: _FabricOpti
                 if len(feature_queue) == len_q:
                     batch_feats = F.normalize(torch.stack(batch_feats, dim=0), dim=1)
                     batch_feats_hard = F.normalize(torch.stack(batch_feats_hard, dim=0), dim=1)
-                    loss_sim = similarity_loss(feature_queue_hard,feature_queue)
+                    # loss_sim = similarity_loss(feature_queue_hard,feature_queue)
+                    loss_sim = similarity_loss(batch_feats_hard, batch_feats)
                     loss_sim = torch.tensor(0., device=batch_feats.device) if loss_sim == -1 else loss_sim
                     feature_queue.extend([f.detach() for f in batch_feats])
                     feature_queue_hard.extend([f.detach() for f in batch_feats_hard])
@@ -225,21 +226,7 @@ def train_resam(cfg: Box, fabric: L.Fabric, model: Model, optimizer: _FabricOpti
                     
                     loss_sim = torch.tensor(0., device=fabric.device)
 
-                # After computing embeddings, hard_embeddings and bboxes
-                # batch_feats      = [get_bbox_feature(embeddings,      bbox) for bbox in bboxes]
-                # batch_feats_hard = [get_bbox_feature(hard_embeddings, bbox) for bbox in bboxes]
-
-                # batch_feats      = F.normalize(torch.stack(batch_feats,      dim=0), dim=1)
-                # batch_feats_hard = F.normalize(torch.stack(batch_feats_hard, dim=0), dim=1)
-
-                # margin = 0.5
-
-                # cos_sim = (batch_feats * batch_feats_hard).sum(dim=1)
-
-                # # pull together if similarity > margin
-                # loss_sim = torch.clamp(1 - cos_sim, min=0)
-
-                
+          
 
                 batch_feats = []  
 
