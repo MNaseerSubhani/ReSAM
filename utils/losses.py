ca8c86daf53b4bd6f1826ee7c46e25c43c35bee0
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-03-17 05:26:58 +0500

    update

--- utils/losses.py
+++ utils/losses.py
@@ -8,25 +8,25 @@ from scipy.optimize import linear_sum_assignment
 ALPHA = 0.8
 GAMMA = 2
 
-# class FocalLoss(nn.Module):
+class FocalLoss(nn.Module):
 
-#     def __init__(self, weight=None, size_average=True):
-#         super().__init__()
+    def __init__(self, weight=None, size_average=True):
+        super().__init__()
 
 
-#     def forward(self, inputs, targets, alpha=ALPHA, gamma=GAMMA, smooth=1):
-#         # inputs = F.sigmoid(inputs)
-#         inputs = torch.clamp(inputs, min=0, max=1)
-#         #flatten label and prediction tensors
-#         inputs = inputs.view(-1)
-#         targets = targets.view(-1)
+    def forward(self, inputs, targets, alpha=ALPHA, gamma=GAMMA, smooth=1):
+        # inputs = F.sigmoid(inputs)
+        inputs = torch.clamp(inputs, min=0, max=1)
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
 
-#         BCE = F.binary_cross_entropy(inputs, targets, reduction='none')
-#         BCE_EXP = torch.exp(-BCE)
-#         focal_loss = alpha * (1 - BCE_EXP)**gamma * BCE
-#         focal_loss = focal_loss.mean()
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='none')
+        BCE_EXP = torch.exp(-BCE)
+        focal_loss = alpha * (1 - BCE_EXP)**gamma * BCE
+        focal_loss = focal_loss.mean()
 
-#         return focal_loss.mean()
+        return focal_loss.mean()
 
 
 class BCELoss(nn.Module):
