a8906fad28bfe912c435bb1ef81d540f03e4eaf2
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-03-18 13:18:12 +0500

    update

--- scripts/train_resam_whu.sh
+++ scripts/train_resam_whu.sh
@@ -5,10 +5,18 @@ load_type="soft"
 num_points_list=(3)
 output_dirs=("work_dir/whu/resam")
 
+script_name="${1}.py"
+
+# Check if the file actually exists before running
+if [ ! -f "$script_name" ]; then
+    echo "Error: File $script_name not found!"
+    exit 1
+fi
+
 for output_dir in "${output_dirs[@]}"; do
     for num_points in "${num_points_list[@]}"; do
         
         out_dir="${output_dir}/point_${num_points}"
-        CUDA_VISIBLE_DEVICES=0 python train_resam.py --cfg "$cfg_file" --prompt "$prompt" --num_points "$num_points" --out_dir "$out_dir"  --load_type "$load_type"
+        CUDA_VISIBLE_DEVICES=0 python "$script_name" --cfg "$cfg_file" --prompt "$prompt" --num_points "$num_points" --out_dir "$out_dir"  --load_type "$load_type"
     done
 done
