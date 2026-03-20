9cdd8b5f2f6fb2f1d0eb540694e5ec49e803aa1f
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-03-20 09:46:14 +0500

    update

--- train_resam.py
+++ train_resam.py
@@ -600,8 +600,8 @@ def train_resam(cfg: Box, fabric: L.Fabric, model: Model, optimizer: _FabricOpti
                         batch_iou = calc_iou(pred_mask, soft_mask)
                         loss_iou += F.mse_loss(iou_prediction, batch_iou, reduction='sum') / num_masks 
 
-                loss_focal = loss_focal / num_masks
-                loss_dice = loss_dice / num_masks
+                # loss_focal = loss_focal / num_masks
+                # loss_dice = loss_dice / num_masks
 
                 del  pred_masks, iou_predictions 
                 del pred_stack, overlap_map, invert_overlap_map
