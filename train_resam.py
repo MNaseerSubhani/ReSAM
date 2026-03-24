afee225245737f03608303479556d27a76aad6c1
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2026-03-24 19:45:21 +0500

    change q len

--- train_resam.py
+++ train_resam.py
@@ -37,13 +37,6 @@ import torch.nn.functional as F
 from collections import deque
 import matplotlib. pyplot as plt
 
-# seed = 42
-# import random
-
-# torch.manual_seed(seed)
-# torch.cuda.manual_seed_all(seed)
-# np.random.seed(seed)
-# random.seed(seed)
 
 
 def process_forward(img_tensor, prompt, model):
@@ -68,7 +61,7 @@ def process_forward(img_tensor, prompt, model):
         
 
 # persistent feature queue
-Q_LEN=256
+Q_LEN=128
 feature_queue = deque(maxlen=Q_LEN)  
 feature_queue_hard = deque(maxlen=Q_LEN)
 
